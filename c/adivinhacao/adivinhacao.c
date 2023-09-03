23c26d505da5c698296ad4ff508416d25b731aeb
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-09-03 15:02:50 -0300

    created itens

---
+++ c/adivinhacao/adivinhacao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("**********************************************\n");
+    printf("*Seja Bem Vindo ao Nosso Jogo de Adivinhacao!*\n");
+    printf("**********************************************\n");
+
+    int numeroSecreto;
+    numeroSecreto = 42;
+
+    //printf("O número secreto é %d", numeroSecreto);
+    
+}
